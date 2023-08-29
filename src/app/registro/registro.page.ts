e9be6aaece9055b1dd636a6c6059c8caa2cd86e7
GZ-IMA <141601763+GZ-IMA@users.noreply.github.com>
2023-08-29 11:45:27 -0600

    Cambios ventana Login

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { 
+import {
   FormGroup,
   FormControl,
   Validators,
@@ -20,27 +20,27 @@ export class RegistroPage implements OnInit {
 
   constructor(public fb: FormBuilder,
     public alertController: AlertController) {
-    
+
     this.formularioRegistro = this.fb.group({
       'nombre': new FormControl("", Validators.required),
       'password': new FormControl("", Validators.required),
       'confirmacionPassword': new FormControl("", Validators.required)
     });
-   }
+  }
 
   ngOnInit() {
   }
 
-  async guardar(){
+  async guardar() {
     var f = this.formularioRegistro.value;
 
-    if(this.formularioRegistro.invalid){
+    if (this.formularioRegistro.invalid) {
       const alert = await this.alertController.create({
         header: 'Datos incompletos',
         message: 'Tienes que llenar todos los datos',
         buttons: ['Aceptar']
       });
-  
+
       await alert.present();
       return;
     }
@@ -50,7 +50,7 @@ export class RegistroPage implements OnInit {
       password: f.password
     }
 
-    localStorage.setItem('usuario',JSON.stringify(usuario));
+    localStorage.setItem('usuario', JSON.stringify(usuario));
   }
 
 }
