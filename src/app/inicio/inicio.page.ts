25fe4f4a68398574eaa693718efbafbf46fbf1e4
GZ-IMA <141601763+GZ-IMA@users.noreply.github.com>
2023-08-31 10:28:07 -0600

    Cambios en Ventana Inicio

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+//import { get } from 'http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +10,99 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  fecha: string;
+
+  entradas!: Array<{
+    fecha: string;
+    fechaTexto: string;
+    texto: string;
+  }>;
+  //
+//
+  entradaActual: {
+    fecha: string;
+    fechaTexto: string;
+    texto: string;
+  } | undefined;
+
+  constructor(public toastController: ToastController) { 
+    moment.locale('es-mx');
+    this.fecha = moment().format();
+    this.cargarEntradas();
+  }
 
   ngOnInit() {
+  }
+
+  cargarEntradas(){
+    var fecha = moment(this.fecha).format('DD-MM-YY');
+
+    //this.entradas = JSON.parse(localStorage.getItem('entradas'));
+    var entradasString = localStorage.getItem('entradas');
+    if (entradasString !== null) {
+      var usuario = JSON.parse(entradasString);
+
+    if(this.entradas){
+      var entradaActual = this.entradas.find((elemento)=>{
+        return elemento.fecha == fecha;
+      });
+      if(entradaActual){
+        this.entradaActual = entradaActual;
+      }else{
+        this.inicializarNuevaEntrada();
+      }
+    }else{
+      this.inicializarNuevaEntrada();
+    }
+  }
+}
+
+  inicializarNuevaEntrada(){
+    var fecha = moment(this.fecha).format('DD-MM-YY');
+    var dia = moment(this.fecha).format('DD');
+    var mes = moment(this.fecha).format('MMMM');
+    var year = moment(this.fecha).format('YYYY');
+
+    this.entradaActual = {
+      fechaTexto: dia + ' de ' + mes + ' del ' +  year,
+      fecha: fecha,
+      texto: ''
+    }
+  }
+
+  async guardar(entradaActual: {
+    fecha: string,
+    fechaTexto: string,
+    texto: string
+  }){
+
+    var fecha = moment(this.fecha).format('DD-MM-YY');
+
+    if(this.entradas){
+      var item = this.entradas.find((elemento)=>{
+        return elemento.fecha == fecha;
+      });
+      if(item){
+        localStorage.setItem('entradas',JSON.stringify(this.entradas));
+      }else{
+        this.guardarItem(entradaActual);
+      }
+
+    }else{
+      this.entradas = [];
+      this.guardarItem(entradaActual);
+    }
+
+    const toast = await this.toastController.create({
+      message: 'Datos guardados',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  guardarItem(entrada:{fecha: string,fechaTexto: string,texto: string }){
+    this.entradas.push(entrada);
+    localStorage.setItem('entradas',JSON.stringify(this.entradas));
   }
 
 }
